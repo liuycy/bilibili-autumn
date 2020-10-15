c220d0fc2f529c32fad2027b573e852d16bf7232
liuycy <liuycy@yeah.net>
2020-10-15 23:27:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <bili-banner></bili-banner>
+  <h1 style="text-align: center">哔哩哔哩 (゜-゜)つロ 干杯~-bilibili</h1>
+  <img class="full-bg" src="/src/assets/full-bg.png" />
+</template>
+
+<script>
+import BiliBanner from './components/BiliBanner.vue'
+
+export default {
+  name: 'App',
+  components: { BiliBanner },
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
+
+<style scoped>
+.full-bg {
+  min-width: 999px;
+  width: 100vw;
+}
+</style>
---
+++ src/components/BiliBanner.vue
@@ -0,0 +1,130 @@
+<template>
+  <div ref="placeholder" class="banner">
+    <img class="banner-placeholder" src="/src/assets/full-bg.png" />
+    <canvas :ref="(el) => (layers.bg = el)" class="banner-layer"></canvas>
+    <canvas :ref="(el) => (layers.twotwo = el)" class="banner-layer"></canvas>
+    <canvas :ref="(el) => (layers.land = el)" class="banner-layer"></canvas>
+    <canvas :ref="(el) => (layers.ground = el)" class="banner-layer"></canvas>
+    <canvas :ref="(el) => (layers.grass = el)" class="banner-layer"></canvas>
+    <canvas
+      :ref="(el) => (layers.threethree = el)"
+      class="banner-layer"
+    ></canvas>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, onMounted, onBeforeMount, watch, nextTick } from 'vue'
+
+const images = reactive({
+  bg: null,
+  twotwo: null,
+  land: null,
+  ground: null,
+  grass: null,
+  threethree: null,
+})
+function buildImage(src) {
+  return new Promise((resolve) => {
+    const image = new Image()
+    image.onload = () => resolve(image)
+    image.src = src
+  })
+}
+onBeforeMount(() => {
+  buildImage('/src/assets/bg.png').then((img) => (images.bg = img))
+  buildImage('/src/assets/22-open-eye.png').then((img) => (images.twotwo = img))
+  buildImage('/src/assets/land.png').then((img) => (images.land = img))
+  buildImage('/src/assets/ground.png').then((img) => (images.ground = img))
+  buildImage('/src/assets/grass.png').then((img) => (images.grass = img))
+  buildImage('/src/assets/33.png').then((img) => (images.threethree = img))
+})
+
+export const placeholder = ref(null)
+
+export const layers = reactive({
+  bg: null,
+  twotwo: null,
+  land: null,
+  ground: null,
+  grass: null,
+  threethree: null,
+})
+function draw(image, config) {
+  const { sx = 0, sy = 0, sw, sh, blur: b } = config || {}
+  return {
+    to(canvas) {
+      const ctx = canvas.getContext('2d')
+      ctx.filter = `blur(${b}px)`
+      ctx.drawImage(image, sx, sy, sw, sh, 0, 0, canvas.width, canvas.height)
+    },
+  }
+}
+function initLayer(canvas) {
+  const { width, height } = placeholder.value.getBoundingClientRect()
+  canvas.width = width
+  canvas.height = height
+}
+onMounted(async () => {
+  initLayer(layers.bg)
+  watch(
+    () => images.bg,
+    (bg) => draw(bg, { sw: 3000, sh: 250, blur: 6 }).to(layers.bg)
+  )
+
+  initLayer(layers.twotwo)
+  watch(
+    () => images.twotwo,
+    (twotwo) => draw(twotwo, { sw: 3000, sh: 275, blur: 0 }).to(layers.twotwo)
+  )
+
+  initLayer(layers.land)
+  watch(
+    () => images.land,
+    (land) => draw(land, { sw: 3000, sh: 250, blur: 5 }).to(layers.land)
+  )
+
+  initLayer(layers.ground)
+  watch(
+    () => images.ground,
+    (ground) => draw(ground, { sx: -100, sw: 3000, sh: 250, blur: 4 }).to(layers.ground)
+  )
+
+  initLayer(layers.grass)
+  watch(
+    () => images.grass,
+    (grass) =>
+      draw(grass, { sx: 150, sw: 3000, sh: 275, blur: 3 }).to(layers.grass)
+  )
+
+  initLayer(layers.threethree)
+  watch(
+    () => images.threethree,
+    (threethree) =>
+      draw(threethree, { sw: 3000, sh: 275, blur: 2 }).to(
+        layers.threethree
+      )
+  )
+})
+</script>
+
+<style lang="scss" scoped>
+.banner {
+  display: flex;
+  width: 100vw;
+  min-width: 999px;
+  position: relative;
+
+  &-placeholder {
+    width: 100%;
+    height: 9.375vw;
+    opacity: 0.3;
+  }
+
+  &-layer {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+</style>
